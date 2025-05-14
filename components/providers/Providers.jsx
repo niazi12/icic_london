25a029827183af20673baab33a865f033fe390e4
sadman <sadman923@gmail.com>
2025-05-14 18:54:21 +0600

    theme  update

--- components/providers/Providers.jsx
+++ components/providers/Providers.jsx
@@ -2,21 +2,14 @@
 
 import { Toaster } from "@/components/ui/sonner";
 // import { SessionProvider } from "next-auth/react";
-import { ThemeProvider } from "./theme-provider";
 
 const Providers = ({ children }) => {
   return (
     // <SessionProvider>
-
-      <ThemeProvider
-          attribute="class"
-          defaultTheme="dark"
-          enableSystem
-          disableTransitionOnChange
-        >
-        <Toaster position="top-center" />
-        {children}
-        </ThemeProvider>
+    <>
+      <Toaster position="top-center" />
+      {children}
+    </>
     // </SessionProvider>
   );
 };
